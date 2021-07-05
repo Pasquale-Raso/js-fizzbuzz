90cfde603881710c5d69af44d47964e73c6f3964
Pasquale-Raso <82545398+Pasquale-Raso@users.noreply.github.com>
2021-07-05 17:10:01 +0200

    fine

--- js/javascript.js
+++ js/javascript.js
@@ -2,16 +2,31 @@
 // ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
 // Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 
-
+var lista = document.getElementById('lista');
+    
 
 for (var i = 1; i <= 100; i++) {
-    if (i % 3 === 0) {
-       console.log('Fizz'); 
-    } else if (i % 5 === 0) {
-        console.log('Buzz'); 
-    } else if ((i % 3 === 0) && (i % 5 === 0)) {
-        console.log('FizzBuzz'); 
-     } else {
-        console.log(i);
-     }
+
+    if ((i % 3 === 0) && (i % 5 === 0)) {
+        // console.log('Fizz Buzz');
+        lista.innerHTML += 'Fizz Buzz '
+    }
+
+    else if (i % 3 === 0) {
+        // console.log('Fizz');
+        lista.innerHTML += 'Fizz '
+    } 
+
+    else if (i % 5 === 0) {
+        // console.log('Buzz');
+        lista.innerHTML += 'Buzz ';
+    } 
+
+    else {
+        lista.innerHTML += i + ' ';
+        // console.log(i);
+    }
 }
+
+
+
